3bcb55a91616dda06a481e71700432197d8d2b84
bangle <lmh3294@naver.com>
2021-10-26 23:48:03 +0900

    백준 입출력과 사칙연산 - Hello World! 출력

--- DailyJava/src/com/bangle/IO/HelloWorld.java
+++ DailyJava/src/com/bangle/IO/HelloWorld.java
@@ -4,7 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		print();
+		
+		
 	}
-
+	public static void print() {
+		
+		
+		System.out.println("Hello World");
+	}
 }
